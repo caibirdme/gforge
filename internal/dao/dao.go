eaf7e58fcb93a95a0925f7286bfb677a36ba617b
forz <forzfuyao@gmail.com>
2019-05-24 19:18:49 +0800

    增加对事务的支持

--- internal/dao/dao.go
+++ internal/dao/dao.go
@@ -27,6 +27,25 @@ const (
 		return res,err
 	}
 
+	//GetOne gets one record from table {{.TableName}} by condition "where" in Tx
+	func (*{{.StructName}})GetOneTx(Tx *sql.Tx, where map[string]interface{}) (*{{.StructName}}, error) {
+		if nil == Tx {
+			return nil, errors.New("sql.DB object couldn't be nil")
+		}
+		cond,vals,err := builder.BuildSelect("{{.TableName}}", where, nil)
+		if nil != err {
+			return nil, err
+		}
+		row,err := Tx.Query(cond, vals...)
+		if nil != err || nil == row {
+			return nil, err
+		}
+		defer row.Close()
+		var res *{{.StructName}}
+		err = scanner.Scan(row, &res)
+		return res,err
+	}
+
 	//GetMulti gets multiple records from table {{.TableName}} by condition "where"
 	func (*{{.StructName}})GetMulti(db *sql.DB, where map[string]interface{}) ([]*{{.StructName}}, error) {
 		if nil == db {
@@ -37,6 +56,25 @@ const (
 			return nil, err
 		}
 		row,err := db.Query(cond, vals...)
+		if nil != err || nil == row {
+			return nil, err
+		}
+		defer row.Close()
+		var res []*{{.StructName}}
+		err = scanner.Scan(row, &res)
+		return res,err
+	}
+
+	//GetMulti gets multiple records from table {{.TableName}} by condition "where" in Tx
+	func (*{{.StructName}})GetMultiTx(Tx *sql.Tx, where map[string]interface{}) ([]*{{.StructName}}, error) {
+		if nil == Tx {
+			return nil, errors.New("sql.DB object couldn't be nil")
+		}
+		cond,vals,err := builder.BuildSelect("{{.TableName}}", where, nil)
+		if nil != err {
+			return nil, err
+		}
+		row,err := Tx.Query(cond, vals...)
 		if nil != err || nil == row {
 			return nil, err
 		}
@@ -62,6 +100,22 @@ const (
 		return result.LastInsertId()
 	}
 
+	//Insert inserts an array of data into table {{.TableName}} in Tx
+	func (*{{.StructName}})InsertTx(Tx *sql.Tx, data []map[string]interface{}) (int64, error) {
+		if nil == Tx {
+			return 0, errors.New("sql.DB object couldn't be nil")
+		}
+		cond, vals, err := builder.BuildInsert("{{.TableName}}", data)
+		if nil != err {
+			return 0, err
+		}
+		result,err := Tx.Exec(cond, vals...)
+		if nil != err || nil == result {
+			return 0, err
+		}
+		return result.LastInsertId()
+	}
+
 	//Update updates the table {{.TableName}}
 	func (*{{.StructName}})Update(db *sql.DB, where,data map[string]interface{}) (int64, error) {
 		if nil == db {
@@ -78,6 +132,22 @@ const (
 		return result.RowsAffected()
 	}
 
+	//Update updates the table {{.TableName}} in Tx
+	func (*{{.StructName}})UpdateTx(Tx *sql.Tx, where,data map[string]interface{}) (int64, error) {
+		if nil == Tx {
+			return 0, errors.New("sql.DB object couldn't be nil")
+		}
+		cond,vals,err := builder.BuildUpdate("{{.TableName}}", where, data)
+		if nil != err {
+			return 0, err
+		}
+		result,err := Tx.Exec(cond, vals...)
+		if nil != err {
+			return 0, err
+		}
+		return result.RowsAffected()
+	}
+
 	// Delete deletes matched records in {{.TableName}}
 	func (*{{.StructName}})Delete(db *sql.DB, where map[string]interface{}) (int64, error) {
 		if nil == db {
@@ -88,6 +158,22 @@ const (
 			return 0, err
 		}
 		result,err := db.Exec(cond, vals...)
+		if nil != err {
+			return 0, err
+		}
+		return result.RowsAffected()
+	}
+
+	// Delete deletes matched records in {{.TableName}} in Tx
+	func (*{{.StructName}})DeleteTx(Tx *sql.Tx, where map[string]interface{}) (int64, error) {
+		if nil == Tx {
+			return 0, errors.New("sql.DB object couldn't be nil")
+		}
+		cond,vals,err := builder.BuildDelete("{{.TableName}}", where)
+		if nil != err {
+			return 0, err
+		}
+		result,err := Tx.Exec(cond, vals...)
 		if nil != err {
 			return 0, err
 		}
